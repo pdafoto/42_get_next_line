606853ed3aa014fc2b1a8890e4ccccdfcfeb30bb
Nicolás <sourcetree@pdafoto.es>
2023-11-16 12:07:22 +0100

    creating files

--- get_next_line.c
+++ get_next_line.c
@@ -1,31 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nperez-d <nperez-d@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/25 18:40:50 by nperez-d          #+#    #+#             */
-/*   Updated: 2023/11/16 11:15:02 by nperez-d         ###   ########.fr       */
+/*   Created: 2023/11/16 11:02:19 by nperez-d          #+#    #+#             */
+/*   Updated: 2023/11/16 11:04:05 by nperez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-# include <stdlib.h>
-# include <unistd.h>
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 5
-# endif
-
-typedef struct s_list
+char	*get_next_line(int fd)
 {
-	void			*content;
-	struct s_list	*next;
-}		t_list;
 
-char	*get_next_line(int fd);
-
-#endif
+}
