c018eeabaa323516041a1c89808fa8d84b7f1df8
Nicolás <sourcetree@pdafoto.es>
2024-04-27 14:52:45 +0200

    removed comments from bonus main

--- main_bonus.c
+++ main_bonus.c
@@ -6,31 +6,13 @@
 /*   By: nperez-d <nperez-d@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 13:39:59 by nperez-d          #+#    #+#             */
-/*   Updated: 2024/04/26 14:17:10 by nperez-d         ###   ########.fr       */
+/*   Updated: 2024/04/27 14:52:27 by nperez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 #include <fcntl.h>
 #include <stdio.h>
-
-// int	main(void)
-// {
-// 	int		fd1;
-// 	int		fd2;
-// 	char	*line;
-
-// 	fd1 = open("test.txt", O_RDONLY);
-// 	fd2 = open("test2.txt", O_RDONLY);
-// 	while ((line = get_next_line(fd1)) || (line = get_next_line(fd2)))
-// 	{
-// 		printf("%s", line);
-// 		free(line);
-// 	}
-// 	close(fd1);
-// 	close(fd2);
-// 	return (0);
-// }
 
 int	main(void)
 {
